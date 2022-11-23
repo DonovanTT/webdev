4d9219a93a2195b3c120b4335f6ae0a70918db22
unknown <donovantthacker@gmail.com>
2022-11-23 17:51:09 -0500

    Changed start script and updated the port.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', {root: __dirname});
